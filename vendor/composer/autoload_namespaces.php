ceabdd520f4f6dd9c289f1f16f55d894a64a31a4
David Callizaya <davidcallizaya@Davids-MacBook-Pro.local>
2016-05-11 15:47:52 -0400

    Change to ps4

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,5 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'ReplaceCode' => array($baseDir . '/src/ReplaceCode'),
 );
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'ReplaceCode\\' => array($baseDir . '/src/ReplaceCode'),
 );
